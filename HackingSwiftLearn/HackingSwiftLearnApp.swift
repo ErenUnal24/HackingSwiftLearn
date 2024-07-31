a827f0333eb572d6d995c301ca2c5c731be7956a
Eren-Unal <bilalunal24@gmail.com>
2024-07-31 09:24:02 +0300

    Initial Commit

---
+++ HackingSwiftLearn/HackingSwiftLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingSwiftLearnApp.swift
+//  HackingSwiftLearn
+//
+//  Created by Eren on 31.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct HackingSwiftLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
